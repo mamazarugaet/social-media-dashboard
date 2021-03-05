24ae71a78f11d95577e59bd7be60e25df9a40efd
mamazarugaet <78613262+mamazarugaet@users.noreply.github.com>
2021-03-05 14:36:14 +0300

    adaptive

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-//var btn = document.getElementById('btn');
-//btn.addEventListener("click", toggle)
-//var el = document.querySelectorAll('.card-item');
-
-
-
-
-//function toggle (){
-//    console.log(el);
-//    el.classList.add('dark-mode-card');
-//}
-
 var btn = document.getElementById('toggle-button');
 var bgc = document.querySelectorAll('.card-item');
+
+
+
 btn.addEventListener("click", function(event) {
-    
     bgc.forEach((el) => {
       el.classList.toggle("dark-mode-card");
     });
